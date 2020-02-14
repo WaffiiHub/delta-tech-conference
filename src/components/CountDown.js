09685b5bf6d58bfb3948cb3dfb53b9f32e853b38
codekyd <okpusjamin@gmail.com>
2020-02-14 11:54:56 +0100

    add countdown and adjust layouts

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,60 +1,122 @@
-import React, { useEffect, useState } from 'react'
-
-const CountDown = (props) => {
-
-    const [state, setState] = useState({
-        days: undefined,
-        hours: undefined,
-        minutes: undefined,
-        seconds: undefined
-
-    })
-    useEffect(() => {
-        const interval = () =>  { setInterval(() => {
-            const { timeTillDate, timeFormat } = props;
-            const then = moment(timeTillDate, timeFormat);
-            const now = moment();
-            const countdown = moment(then - now);
-            const days = countdown.format('D');
-            const hours = countdown.format('HH');
-            const minutes = countdown.format('mm');
-            const seconds = countdown.format('ss');
-
-            setState({ days, hours, minutes, seconds });
+import React from 'react';
+class Countdown extends React.Component {
+	state = {
+		days: undefined,
+		hours: undefined,
+		minutes: undefined,
+		seconds: undefined
+	}
+	
+	componentDidMount() {
+		this.interval = setInterval(() => {
+            const then = new Date('Aug 22 2020 9:00:00').getTime()
+            const now = new Date().getTime();
             
-        }, 1000);
+            const countdown = then - now;
+            console.log(countdown);
+            const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+          
 
-    }
+			this.setState({ days, hours, minutes, seconds });
+		}, 1000);
+	}
 
-        return () => {
-            clearInterval(interval())
-        }
+	componentWillUnmount() {
+		if(this.interval) {
+			clearInterval(this.interval);
+		}
+	}
+	
+	render() {
+		const { days, hours, minutes, seconds } = this.state;
+		const daysRadius = mapNumber(days, 24, 0, 0, 360);
+		const hoursRadius = mapNumber(hours, 24, 0, 0, 360);
+		const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
+		const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
 
-    })
-    const { days, hours, minutes, seconds } = state;
-    return (
-        <Fragment>
-        <h1>Countdown</h1>
-        <div className="countdown-wrapper">
-            <div className="countdown-item">
-                {days}
-                <span>days</span>
-            </div>
-            <div className="countdown-item">
-                {hours}
-                <span>hours</span>
-            </div>
-            <div className="countdown-item">
-                {minutes}
-                <span>minutes</span>
-            </div>
-            <div className="countdown-item">
-                {seconds}
-                <span>seconds</span>
-            </div>
-        </div>
-    </Fragment>
-    )
+		if(!seconds) {
+			return null;
+		}
+		
+		return (
+			<div className="countDown">
+				<h1>Coming Soon</h1>
+				<div className='countdown-wrapper'>
+					{days && (
+						<div className='countdown-item'>
+							<SVGCircle radius={daysRadius} />
+							{days} 
+							<span>days</span>
+						</div>
+					)}
+					{hours && (
+						<div className='countdown-item'>							
+							<SVGCircle radius={hoursRadius} />
+							{hours} 
+							<span>hours</span>
+						</div>
+					)}
+					{minutes && (
+						<div className='countdown-item'>
+							<SVGCircle radius={minutesRadius} />
+							{minutes} 
+							<span>minutes</span>
+						</div>
+					)}
+					{seconds && (
+						<div className='countdown-item'>
+							<SVGCircle radius={secondsRadius} />
+							{seconds} 
+							<span>seconds</span>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
-export default CountDown
+const SVGCircle = ({ radius }) => (
+	<svg className='countdown-svg'>
+		<path fill="none" stroke="#fff" stroke-width="4" d={describeArc(50, 50, 48, 0, radius)}/>
+	</svg>
+);
+
+
+
+// From stackoverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle){
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    var d = [
+        "M", start.x, start.y, 
+        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
+    return d;       
+}
+
+// Stackoverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function mapNumber(number, in_min, in_max, out_min, out_max) {
+  return (number - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+
+export default Countdown;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,43 +3,19 @@ import { Link, useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import moment from 'moment';
+import Countdown from '../components/CountDown'
 
 const IndexPage = () => {
-  const then = moment(timeTillDate, timeFormat);
-  const now = moment();
-  const countdown = moment(then - now);
-  const days = countdown.format('D');
-  const hours = countdown.format('HH');
-  const minutes = countdown.format('mm');
-  const seconds = countdown.format('ss');
-
-
-  const [countDown, setCountDown] = useState({
-    days: undefined,
-    hours: undefined,
-    mins : undefined,
-    secs: undefined
-  }) 
-
+ 
 
   
   return(
-    <Layout>
+    <Layout>  
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-
-  <h5> Days : {days}</h5>
-  <h5> Hours : {hours}</h5>
-  <h5> Minutes : {mins}</h5>
-  <h5> Seconds : {secs}</h5>
-
-<h4>{ moment().format('MMMM Do YYYY, h:mm:ss a') }</h4>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>The biggest Tech   conference in Niger Delta </h1>
+   
+	<Countdown/>
+  
   </Layout>
 
   )
