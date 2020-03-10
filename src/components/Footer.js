f2c763d8cef29cf6679ac9a5e67bc73eb2eb27af
codekyd <okpusjamin@gmail.com>
2020-03-10 11:53:54 +0100

    checking for responsiveness

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+       <Container>
+       <p>My Footer</p>
+       </Container>
+            
+        </footer>
+    )
+}
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,31 @@ const IndexPage = () => {
     
 
   }, [])
+
+
+  const data = useStaticQuery(graphql`
+  query {
+    speaker1: file(relativePath: { eq: "speaker.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    },
+    wtnLogo: file(relativePath: { eq: "wtn.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+
+
+
+  }
+`)
+
+
  
 
   return(
@@ -30,12 +55,33 @@ const IndexPage = () => {
     <Container>
       <div className="header">
       <Row className="">
-        <Col md="6 mt-3">
-  <h1>The <span className="text-success echo"></span> Tech conference in Niger Delta  </h1>
+        <Col md="7">
+  <h1>The <span className="text-success echo"></span> <br/>Tech conference in The Niger Delta  </h1>
 
-        <h6 className="mt-3"> August 14th-15th 2020</h6>
+
+<Row>
+  <Col md="4 p-0">
+  <h6 className="mt-3">  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+          <span className="ml-2">August 14th-15th 2020</span> 
+     </h6>
+  </Col>
+
+  <Col md="6 p-0">
+
+  <h6 className="mt-3">
+     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
+
+     <span className="ml-2">Brown Hill Hotel, Warri Nigeria.</span> 
+
+
+     </h6>
+
+  </Col>
+</Row>
+      
+   
         </Col>
-        <Col md="6">
+        <Col md="5">
        <div className="p-3">
 
        <p className="float-left"> " Voluptate Laborum deserunt fugiat irure culpa laboris culpa culpa dolore ipsum Lorem."</p>
@@ -53,23 +99,23 @@ const IndexPage = () => {
 	{/* <Countdown/> */}
     <div className="stats">
     <Row>
-    <Col sm="3">
+    <Col xs="6" md="3">
       <h1>3,000+</h1>
       <p>Attendees</p>
     </Col>
 
-    <Col sm="3">
+    <Col xs="6" md="3">
       <h1>30+</h1>
       <p>Speakers</p>
     </Col>
 
 
-    <Col sm="3">
-      <h1>300+</h1>
+    <Col xs="6" md="3">
+      <h1>25+</h1>
       <p>Exhibitors</p>
     </Col>
 
-    <Col sm="3">
+    <Col xs="6" md="3">
       <h1>400+</h1>
       <p>Gamers</p>
     </Col>
@@ -116,19 +162,104 @@ The New York Times that we assemble “a grand conclave of the tech industry’s
    </section>
 
    <section>
+   <>
+     
+   </>
+   </section>
+
+   <section className="speakers">
      <Container>
        <h1>Some keynote speakers for <span className="text-success">2020</span></h1>
 
        <Row>
-         <Col md="6">
+         <Col md="6" className="p-1">
+          <div className="">
+
+          <Img fluid={data.speaker1.childImageSharp.fluid}/>
+      
+          
+          </div>
+         
          </Col>
 
-         <Col md="6">
+         <Col md="6" className="p-0">
+           <Row>
+             <Col xs="6" md="6" className="p-0">
+
+
+       <Img fluid={data.speaker1.childImageSharp.fluid}/>
+
+             </Col>
+             <Col xs="6" md="6" className="p-0">
+
+
+       <Img fluid={data.speaker1.childImageSharp.fluid}/>
+
+             </Col>
+             <Col xs="6" md="6" className="p-0">
+
+
+       <Img fluid={data.speaker1.childImageSharp.fluid}/>
+
+             </Col>
+             <Col xs="6" md="6" className="p-0">
+
+
+       <Img fluid={data.speaker1.childImageSharp.fluid}/>
+
+             </Col>
+           </Row>
+       {/* <div className="speaker__small"></div>
+
+       <div className="speaker__small">
+         <Img fluid={data.speaker1.childImageSharp.fluid}/></div>
+         <div className="speaker__small">
+         <Img fluid={data.speaker1.childImageSharp.fluid}/></div>
+         <div className="speaker__small">
+         <Img fluid={data.speaker1.childImageSharp.fluid}/></div> */}
+
          </Col>
        </Row>
     
      </Container>
    </section>
+
+   <section className="sponsors">
+     <Container>
+
+   <h1>Some of our <span className="text-success">Sponsors</span></h1>
+   <Row className="mt-5">
+     <Col md="2" xs="6">
+     <Img  style={{ width: '180px' }} fluid={data.wtnLogo.childImageSharp.fluid}/>
+     </Col>
+
+     <Col md="2" xs="6">
+     <Img  style={{ width: '180px' }} fluid={data.wtnLogo.childImageSharp.fluid}/>
+     </Col>
+   </Row>
+   <Row>
+     <Col></Col>
+   </Row>
+     </Container>
+   </section>
+
+
+   <section>
+     <Container>
+
+   <h1>Some of our <span className="text-success">Partners</span></h1>
+     </Container>
+   </section>
+
+
+   <section>
+     <Container>
+
+   <h1>Some Exhibitors for <span className="text-success">2020</span></h1>
+     </Container>
+   </section>
+
+
 
 
   
